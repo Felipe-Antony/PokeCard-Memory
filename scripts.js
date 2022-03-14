ffe744f68e96225971c38fd7b6f973aad48e615c
Felipe Antony <felipe.antonio.santos@ccc.ufcg.edu.br>
2022-03-14 14:38:03 -0300

    Add Barra Progresso

--- scripts.js
+++ scripts.js
@@ -11,8 +11,24 @@ let posicaoCartaVirada = -1;
 let valorCartaVirada = 0;
 let pontos = 0;
 
-onload = () => {
+//Barra de Timer/Progresso
+var barraProgress = new Object();
+barraProgress.porcentagem = 0;
 
+barraProgress.carregaBarra = function () {
+    
+    if (barraProgress.porcentagem > 100)
+    barraProgress.porcentagem = 0;
+    
+    $(".barra-progresso").css("width", barraProgress.porcentagem + "%")
+    
+    barraProgress.porcentagem +=2;
+    setTimeout(barraProgress.carregaBarra, 1000);
+}
+
+
+
+onload = () => {
     //Carrega fundo
     let elemImagens = document.querySelectorAll('#memoria img');
     elemImagens.forEach((img, i) => {
@@ -30,6 +46,10 @@ onload = () => {
 //--------------------------------------
 const iniciaJogo = () => {
 
+    $(function () {
+        barraProgress.carregaBarra();
+    })
+
     //embaralhar as cartas
     for(let i=0; i<cartas.length; i++) {
         let p = Math.trunc( Math.random() * cartas.length );
@@ -44,6 +64,7 @@ const iniciaJogo = () => {
     elemImagens.forEach((img, i) => {
         img.onclick = trataCliqueImagens;
         img.style.opacity = 1;
+        img.src = fundo;
     });
 
     //Reseta o estado do jogo
@@ -84,14 +105,12 @@ const trataCliqueImagens = (e) => {
                 img.src = fundo;
                 img.onclick = trataCliqueImagens;
                 cliquesTravados = false;
-            }, 1500);
+            }, 1000);
         }
         temCartaVirada = false;
         posicaoCartaVirada = -1;
         valorCartaVirada = 0;
     }    
-
-    
 
     if(pontos==8) {
         document.querySelector('#btInicio').disabled = false;
